38a7474bbc8fe3e763bc3da79c8a07bc849e8af9
Daniel Rivera <plasx@live.com>
2019-12-25 20:35:58 -0500

    replaced python Exceptions with GraphQLError exception

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 from .models import Track, Like
 from users.schema import UserType
 
@@ -36,7 +37,7 @@ class CreateTrack(graphene.Mutation):
     def mutate(self, info, title, description, url):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Login to get a track')
+            raise GraphQLError('Login to get a track')
 
         track = Track(title=title, description=description, url=url, posted_by=user)
 
@@ -58,7 +59,7 @@ class UpdateTrack(graphene.Mutation):
         track = Track.objects.get(id=track_id)
 
         if track.posted_by != user:
-            raise Exception('Not permitted to update this track.')
+            raise GraphQLError('Not permitted to update this track.')
 
         track.title = title
         track.description = description
@@ -80,7 +81,7 @@ class DeleteTrack(graphene.Mutation):
         track = Track.objects.get(id=track_id)
 
         if track.posted_by != user:
-            raise Exception('Not permitted to delete this track.')
+            raise GraphQLError('Not permitted to delete this track.')
 
         track.delete()
 
@@ -97,12 +98,12 @@ class CreateLike(graphene.Mutation):
     def mutate(self, info, track_id):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Login to like tracks.')
+            raise GraphQLError('Login to like tracks.')
 
         track = Track.objects.get(id=track_id)
 
         if not track:
-            raise Exception('Cannot find track with given track id')
+            raise GraphQLError('Cannot find track with given track id')
 
         Like.objects.create(
             user=user,
