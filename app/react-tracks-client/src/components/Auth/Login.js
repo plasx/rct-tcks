cdf26d3ebb887a2273aa25d87439f6491380c775
Daniel Rivera <plasx@live.com>
2019-12-27 20:24:25 -0500

    created login mutation

--- app/react-tracks-client/src/components/Auth/Login.js
+++ app/react-tracks-client/src/components/Auth/Login.js
@@ -1,17 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import { Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
 import withStyles from "@material-ui/core/styles/withStyles";
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Lock from "@material-ui/icons/Lock";
+import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+import Gavel from "@material-ui/icons/Gavel";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Lock from "@material-ui/icons/Lock";
+
+import Error from "../Shared/Error";
 
 const Login = ({ classes, setNewUser }) => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event, tokenAuth) => {
+    event.preventDefault();
+    tokenAuth();
+  };
+
+  return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <Lock />
+          </Avatar>
+          <Typography variant="title">
+            Login as existing User
+          </Typography>
+
+          <Mutation mutation={LOGIN_MUTATION}
+            variables={{ username, password }}
+            onCompleted={data => {
+              console.log({ data })
+            }}
+          >
+            {(tokenAuth, {loading, error}) => {
+
+              return (
+                  <form onSubmit={event => handleSubmit(event, tokenAuth)} className={classes.form}>
+                    <FormControl margin="normal" required fullWidth>
+                      <InputLabel htmlFor="username">Username</InputLabel>
+                      <Input id="username" onChange={event => setUsername(event.target.value)} />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                      <InputLabel htmlFor="password">Password</InputLabel>
+                      <Input id="password" type="password" onChange={event => setPassword(event.target.value)}/>
+                    </FormControl>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      disabled={
+                        loading ||
+                        !username.trim() ||
+                        !password.trim()
+                      }
+                      className={classes.submit}
+                    >
+                      {loading ? "Logging In. . ." : "Logged In"}
+                    </Button>
+                    <Button
+                      onClick={() => setNewUser(true)}
+                      color="secondary"
+                      variant="outlined"
+                      fullWidth
+                    >
+                      New User? Register here
+                    </Button>
+                    {/* Error handling */}
+                    {error && <Error error={error}/>}
+                  </form>
+
+              );
+            }}
+          </Mutation>
+        </Paper>
+      </div>
+  )
 };
+
+const LOGIN_MUTATION = gql`
+
+mutation ($username: String!, $password: String!){
+  tokenAuth(username: $username, password: $password){
+    token
+  }
+}
+`
 
 const styles = theme => ({
   root: {
--- app/react-tracks-client/src/components/Shared/Error.js
+++ app/react-tracks-client/src/components/Shared/Error.js
@@ -16,7 +16,7 @@ const Error = ({ classes, error }) => {
        }
    />
   )
-}; 
+};
 
 const styles = theme => ({
   snackbar: {
