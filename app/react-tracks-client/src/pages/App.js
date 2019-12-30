3bf836aad767012260c9083275d06367ed293671
Daniel Rivera <plasx@live.com>
2019-12-30 08:07:57 -0500

    added createtrack pop up dialog

--- app/react-tracks-client/src/pages/App.js
+++ app/react-tracks-client/src/pages/App.js
@@ -1,20 +1,49 @@
 import React from "react";
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
 import withStyles from "@material-ui/core/styles/withStyles";
 
 
 import SearchTracks from "../components/Track/SearchTracks";
 import TrackList from "../components/Track/TrackList";
 import CreateTrack from "../components/Track/CreateTrack";
+import Loading from '../components/Shared/Loading';
+import Error from '../components/Shared/Error';
 
 const App = ({ classes }) => {
   return (
       <div className={classes.container}>
         <SearchTracks />
         <CreateTrack />
-        <TrackList />
+        <Query query={GET_TRACKS_QUERY}>
+          {({ data, loading, error }) => {
+            if (loading) return <Loading />
+            if (error) return <Error error={error} />
+
+            return <TrackList tracks={data.tracks} />
+          }}
+        </Query>
       </div>
   )
 };
+
+const GET_TRACKS_QUERY =  gql`
+  query getTracksQuery {
+    tracks {
+      id
+      title
+      description
+      url
+      likes {
+        id
+      }
+      postedBy {
+        id
+        username
+      }
+    }
+  }
+`
 
 const styles = theme => ({
   container: {
